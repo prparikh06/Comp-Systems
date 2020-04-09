8802a34639a74c6f13ffbba79596f9c78cd33ea2
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2020-04-09 18:55:31 -0400

    Update threads.c

--- Project 2/Problem 1/threads.c
+++ Project 2/Problem 1/threads.c
@@ -6,19 +6,31 @@
 
 int mini_array_size;
 pthread_t threads[];
+int global_index = 0;
 
-void* thread_serach2(void* args){
+void* thread_serach(void* args){
+    int* array = (int*) args;
+    int i, n, max;
+    n = sizeof(array)/sizeof(int); //can't guarantee that all array sizes will be same right (if our num workers doesnt divide)
+    max = array[0];
+    global_index++;
+    for (i = 1; i < n; i++){
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] == -50){ //hidden key found
+            printf("hidden key found at %d!\n", global_index);
+        }
+        global_index++;
 
-}
+    }
 
-void* thread_search1(void* args){
 
 }
 
 int main(int argc, char* argv[]){ 
     //if argc, that's how many threads 
-    int numWorkers, i;
-    FILE fp* = fopen("1k_test.txt",  "r");
+    int numWorkers, i,j,n;
+    FILE fp* = fopen("test.txt",  "r"); //TODO this will change
     if (fp == NULL) return -1;
 
     if (argc > 0){
@@ -26,21 +38,39 @@ int main(int argc, char* argv[]){
         threads[numWorkers];
         for (i = 0; i < numWorkers; i++){
             //create threads 
-            mini_array_size = ceil(1000.00/numWorkers); //TODO this will change
+            mini_array_size = ceil(20.00/numWorkers); //TODO this will change
 
         }
     }
 
     else{
         //call recursively for pieces of 10
-        numWorkers = 1000/10; //TODO this will change
+        numWorkers = 20/10; //TODO this will change
         threads[numWorkers];
         mini_array_size = 10;
-        while(fscanf("%d\n") != EOF){
-
+    }
+    for (i = 0; i < numWorkers; i++){ 
+        pthread_t curr_thread;
+        //create mini array
+        int* mini_array = (int*) malloc(sizeof(int) * mini_array_size);
+        for (j = 0; j < mini_array_size; j++){
+            if (fscanf("%d\n", &n) == EOF) break;
+            mini_array[j] = n;
         }
-
+        //mini array done, time to thread
+        pthread_create(&curr_thread, NULL, thread_serach, (void*) mini_array);
+        threads[i] = curr_thread;
     }
 
+    int finalMaximums[numWorkers];
+    //join all threads
+    for (i = 0; i < numWorkers; i++){
+        int* return_vals = (int*) malloc(sizeof(int) * 2);
+        //RETURN VALUES: [max, index of hidden key (if not found, do -1)]
+        pthread_join(threads[i], (void*) &return_vals);
+        printf("thread %d has return values of %d, and %d\n", (int) return_vals[0], (int) return_vals[1]);
+    }
 
 }
+
+
