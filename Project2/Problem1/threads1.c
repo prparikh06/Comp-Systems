61611f5a701a7a4b227a8f7f49fab961597b42b9
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2020-04-10 17:47:13 -0400

    done ???

--- Project2/Problem1/threads1.c
+++ Project2/Problem1/threads1.c
@@ -106,7 +106,7 @@ int main(int argc, char* argv[]){
         pthread_join(threads[i], (void*)&ret);
         
         if(ret != NULL && ret!= -1){
-            //printf("Hi I am Pthread %u and I found the hidden key in position A[%d]\n", &threads[i], ret); 
+            printf("Hi I am Pthread %u and I found the hidden key in position A[%d]\n", &threads[i], ret); 
             found++;
             
         }
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]){
     //end timing
     gettimeofday(&end,NULL);
     float runTime = (float) end.tv_usec - start.tv_usec + 1000000*(end.tv_sec - start.tv_sec);
-    printf("Time of execution to check %s items with %d threads: %f usec\n", argv[4], numWorkers, runTime);
+    //printf("Time of execution to check %s items with %d threads: %f usec\n", argv[4], numWorkers, runTime);
 
     
 
