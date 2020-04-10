a7f32c9f5be5adad0c2314cfb1a6108249c1b837
Priya Parikh <pp649@assembly.cs.rutgers.edu>
2020-04-09 22:33:18 -0400

    just need to test

--- Project2/Problem1/threads.c
+++ Project2/Problem1/threads.c
@@ -19,14 +19,13 @@ void* thread_serach(void* args){
     max = -1;
     //printf("size of mini array: %d\n", n);
     // global_index++;
-    for (i = 2; i < n; i++, j++){
+    for (i = 2; i < n+2; i++, j++){ //n+2 because we need to account for the first 2 extra elements
         if (array[i] > max)
             max = array[i];
         if (array[i] == -50){ //hidden key found
             printf("hidden key found at %d!\n", j);
             found_key = j;
         }
-
     }
 
     pthread_mutex_unlock(&mutex);
@@ -40,8 +39,9 @@ void* thread_serach(void* args){
 int main(int argc, char* argv[]){ 
     //global_index = 0;
     //if argc, that's how many threads 
-
-    int size = 20; //TODO THIS WILL CHANGE
+    
+    time_t start,end;
+    int size = 20, piece_size = 10; //TODO THIS WILL CHANGE
 
     int numWorkers, i,j,k,n;
     FILE* fp = fopen("test.txt",  "r"); //TODO this will change
@@ -49,16 +49,15 @@ int main(int argc, char* argv[]){
 
     if (argc > 1){
         numWorkers = atoi(argv[1]);
+        if (numWorkers > size) numWorkers = 20;
         for (i = 0; i < numWorkers; i++){
             //create threads 
-            mini_array_size = ceil(double(size)/numWorkers); 
-
+            mini_array_size = ceil((double)size/numWorkers); 
         }
     }
-
     else{
-        //call recursively for pieces of 10
-        numWorkers = size/10; 
+        //divide by pieces size
+        numWorkers = size/piece_size; 
         mini_array_size = 10;
     }
     
@@ -66,24 +65,25 @@ int main(int argc, char* argv[]){
     int maximums[numWorkers];
     int finalMax = -1, max_threadID = 0;
 
-    for (i = 0, j = 0, k = 0; i < size, k < numWorkers; i+=j, k++){  //this is a little OD but it's fine
-
+    //start timing
+    time(&start);
+    
+    for (i = 0, k = 0; k < numWorkers; i+=j-2, k++){  //this is a little OD but it's fine
         pthread_t curr_thread;
         //create mini array
-        int* mini_array = (int*) malloc(sizeof(int) * (mini_array_size) + 2); //adding 2 in order to add size and starting index
-        
-        for (j = 2; j < mini_array_size; j++){
+        int* mini_array = (int*) malloc(sizeof(int) * (mini_array_size + 2)); //adding 2 in order to add size and starting index
+        for (j = 2; j < mini_array_size+2; j++){
             if (fscanf(fp,"%d\n", &n) == EOF) break;
             //printf("adding item %d to mini array\n", n);
             mini_array[j] = n;
         }
-        mini_array[0] = j; //size of array
-        mini_array[1] = i; //starting index
 
+        mini_array[0] = j-2; //size of array
+        mini_array[1] = i; //starting index
+        //printf("i = %d, j = %d\n", i,j);
         //mini array done, time to thread
         pthread_create(&curr_thread, NULL, thread_serach, (void*) mini_array);
         threads[k] = curr_thread;
-        
     }
     //join and get absolute max
     for (i = 0; i < numWorkers; i++){
@@ -95,13 +95,15 @@ int main(int argc, char* argv[]){
         printf("thread %u has return values of %d, and %d\n", &threads[i], (int) return_vals[0], (int) return_vals[1]); 
         if (maximums[i] > finalMax){
             finalMax = maximums[i];
-            max_threadID = threads[i];
+            max_threadID = &threads[i];
         }
     }
 
-    printf("The final maximum is %d, discovered by thread %u\n", finalMax, max_threadID);
-
+    time(&end);
     fclose(fp);
+
+    printf("The final maximum is %d, discovered by thread %u\ntime of execution: %f\n", finalMax, max_threadID, (double) (end-start)/CLOCKS_PER_SEC);
+
     
 
 
